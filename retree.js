f5a69744e4ea99c0df556df41f31e9435ac34535
steph <stephanzag@gmail.com>
2011-07-16 23:43:19 -0700

    Plugin container

--- retree.js
+++ retree.js
@@ -9,69 +9,21 @@ var mainData;
 var postOwner = 66749591;
 var postId = 5796;
 var root = {};
-
 root['root_owner_id'] = postOwner;
 root['root_post_id'] = postId;
 root['repost_users'] = [];
 root['all_users'] = [];
-
-
 /*
-VK.Api.call('likes.getList', {
-		type: 'post',
-		owner_id: postOwner, 
-		item_id: postId,
-		test_mode: 1
-	    }, function(r) {
-		root['root_likes_count'] = r.response['count'];
-		root['all_users'] = r.response['users'];
-		var users = r.response['users'];
-		var index = 0;
-			//console.log(r); 
-			var i=0;
-while(i<users.length)
-{
-			console.log(users[i]); 
-			VK.Api.call('wall.get', { 
-					owner_id: users[1],
-					test_mode: 1
-				    }, function(e) {
-						if(e)
-						{
-						console.log(e.response);
-						
-					}
-				    	}
-		);
-		i++;
-		
-}
-	/*	
-for(i=0;i<users.length;i++)
-{
-			VK.Api.call('wall.get', { 
-					owner_id: users[i],
-					test_mode: 1
-				    }, function(e) { 
-					mainData = e.response;
-					if (e.response == undefined)
-					    continue;
-					mainData.forEach(
-					    function(e)
-					    { 
-							if (e['copy_owner_id'] == postOwner)
-							if(e['copy_post_id']== postId)
-							{
-								root['repost_users'].push(e);
-								console.log(e); 
-								console.log(root['repost_users']);
-							}
-					 		});	
-});
-}
+VK.Api.call('wall.get', {test_mode: 1}, function(e) {
+	alert(e.response);
+mainData = e.response;
+mainData.forEach(
+function(e) {});
+
+
+
 });
 */
-
 VK.Api.call('likes.getList', {
 		type: 'post',
 		owner_id: postOwner, 
@@ -103,15 +55,10 @@ VK.Api.call('likes.getList', {
 					    function(e)
 					    { 
 						if (e['copy_owner_id'] == postOwner)
-							if(e['copy_post_id']== postId)
-							{
-								root['repost_users'].push(e);
-								console.log(e); 
-								console.log(root['repost_users']);
-							}
+						    root['repost_users'].push(e);
+						    console.log(e); 
 					    });
 				    });
 			index++;
 		    }, 2000);
 	    });
-
