36f913bb1c8c2ae36bd262d844b3b12a00cd4403
mad <owner.mad.epa@gmail.com>
2011-07-30 23:55:38 +0400

    Fix some bugs with cycles, add some heuristics.
    And fix plotting code

--- retree.js
+++ retree.js
@@ -4,6 +4,7 @@ VK.Widgets.Auth("vk_auth", {
     }
 });
 
+// 1000 likes
 var root_owner_id = 66749591;
 var root_item_id = 5796;
 
@@ -20,32 +21,40 @@ VK.Api.call('wall.getById', {
 
 }, function(r) {
     if (r.error) {
-	console.log("wall.getById error: " + r.error.error_msg);
-	return;
+        console.log("wall.getById error: " + r.error.error_msg);
+        return;
     }
 
     root.data = r.response[0];
 });
 
+var fetched_users = [];
+
 function getTree(root_node, parent)
 {
     console.log("Fetching data from "
-		+ root_node.owner_id + "_" + root_node.item_id);
+                + root_node.owner_id + "_" + root_node.item_id);
+
+    if (fetched_users.indexOf(root_node.owner_id) != -1) {
+        console.log("This user already fetched " + root_node.owner_id);
+        return;
+    }
+
+    fetched_users.push(root_node.owner_id);
 
     VK.Api.call('likes.getList', {
         type: 'post',
         owner_id: root_node.owner_id,
         item_id: root_node.item_id,
-	count: 1000,
+        count: 1000,
         test_mode: 1
-    }, function(r) {
+    }, function(like_list) {
+        if (like_list.error) {
+            console.log("likes.getList error: " + like_list.error.error_msg);
+            return;
+        }
 
-	if (r.error) {
-	    console.log("likes.getList error: " + r.error.error_msg);
-	    return;
-	}
-
-        var users = r.response.users;
+        var users = like_list.response.users;
         var index = 0;
 
         // Iterate for all users which like it and wait
@@ -55,40 +64,50 @@ function getTree(root_node, parent)
             {
                 if (users[index] === undefined) {
                     clearInterval(t);
-		    console.log("Fetched " + index + " users, " + t + " canceled");
-		    // localStorage.setItem(root.owner_id + '_' + root.item_id,
-		    //                      JSON.stringify(root));
+                    console.log("Fetched " + index + " users, " + t + " canceled");
+                    // localStorage.setItem(root.owner_id + '_' + root.item_id,
+                    //                      JSON.stringify(root));
+                    return;
+                }
+
+                if (users[index] == root_node.owner_id) {
+                    index++;
+                    console.log("It is same user, skip it " + root_node.owner_id);
                     return;
                 }
 
                 VK.Api.call('wall.get', {
                     owner_id: users[index],
-		    filter: owner,
                     test_mode: 1
-                }, function(r) {
-		    if (r.error) {
-			console.log("wall.get error: " + r.error.error_msg);
-			return;
-		    }
+                }, function(wall) {
+                    if (wall.error) {
+                        console.log("wall.get error: " + wall.error.error_msg);
+                        return;
+                    }
 
-		    console.log("Fetching " + r.response[0] + " msg, from " + r.response[1].from_id);
+                    if (wall.response[0] == 0) {
+                        console.log("0 messages returned");
+                        return;
+                    }
 
-                    r.response.forEach(
-                        function(r)
+                    console.log("Fetching " + wall.response[0] + " msg, from " + wall.response[1].from_id);
+
+                    wall.response.forEach(
+                        function(msg)
                         {
-                            if (r.copy_owner_id == root_owner_id
-                                && r.copy_post_id == root_item_id)
+                            if (msg.copy_owner_id == root_owner_id
+                                && msg.copy_post_id == root_item_id)
                             {
-				var child = {};
+                                var child = {};
 
-				child.owner_id = r.from_id;
-				child.item_id = r.id;
-				child.data = r;
-				child.childs = [];
+                                child.owner_id = msg.from_id;
+                                child.item_id = msg.id;
+                                child.data = msg;
+                                child.childs = [];
 
-				console.log("Parent has " + parent.length + " childs");
+                                console.log("Parent has " + parent.length + " childs");
 
-				if (parent !== undefined)
+                                if (parent !== undefined)
                                     parent.push(child);
 
                                 setTimeout(getTree(child, child.childs), 1000);
@@ -102,121 +121,55 @@ function getTree(root_node, parent)
 
 getTree(root, root.childs);
 
-var w = 960,
-h = 500,
-rootTree = {},
-data = [rootTree],
-tree = d3.layout.tree().size([w - 20, h - 20]),
-diagonal = d3.svg.diagonal(),
-duration = 750
-//    timer = setInterval(update, duration);
+function plot()
+{
+    var r = 960 / 2;
 
-var vis = d3.select("body").append("svg:svg")
-    .attr("width", w)
-    .attr("height", h)
-    .append("svg:g")
-    .attr("transform", "translate(10, 10)");
+    var tree = d3.layout.tree()
+        .size([360, r - 120])
+        .separation(function(a, b) { return (a.parent == b.parent ? 1 : 2) / a.depth; })
+        .children(function(d) {
+            if (d.childs.length == 0)
+                return null;
+            else
+                return d.childs;});
 
-vis.selectAll("circle")
-    .data(tree(rootTree))
-    .enter().append("svg:circle")
-    .attr("class", "node")
-    .attr("r", 3.5)
-    .attr("cx", x)
-    .attr("cy", y);
+    var diagonal = d3.svg.diagonal.radial()
+        .projection(function(d) { return [d.y, d.x / 180 * Math.PI]; });
 
+    var vis = d3.select("body").append("svg:svg")
+        .attr("width", r * 2)
+        .attr("height", r * 2 - 150)
+        .append("svg:g")
+        .attr("transform", "translate(" + r + "," + r + ")");
 
-var w = 960,
-h = 500,
-r = d3.scale.sqrt().domain([0, 20000]).range([0, 20]);
+    function treemap(json) {
+        var nodes = tree.nodes(json);
 
-var force = d3.layout.force()
-    .gravity(.01)
-    .charge(-120)
-    .linkDistance(60)
-    .size([w, h]);
+        var link = vis.selectAll("path.link")
+            .data(tree.links(nodes))
+            .enter().append("svg:path")
+            .attr("class", "link")
+            .attr("d", diagonal);
 
-var svg = d3.select("body").append("svg:svg")
-    .attr("width", w)
-    .attr("height", h);
+        var node = vis.selectAll("g.node")
+            .data(nodes)
+            .enter().append("svg:g")
+            .attr("class", "node")
+            .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")"; });
 
+        node.append("svg:circle")
+            .attr("r", 4.5);
 
-function update() {
+        node.append("svg:text")
+            .attr("dx", function(d) { return d.x < 180 ? 8 : -8; })
+            .attr("dy", ".31em")
+            .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
+            .attr("transform", function(d) { return d.x < 180 ? null : "rotate(180)"; })
+            .attr("xlink:href", "http://vkontakte.ru/")
+            .text(function(d) { return d.owner_id; });
+    }
 
-    if (data.length >= 500) return clearInterval(timer);
+    treemap(root);
 
-    // Add a new datum to a random parent.
-    // if (root['repost_users'].length == 0)
-    //  return;
-
-    // data = [];
-    // root['repost_users'].forEach(
-    //  function(e) {
-    //      data.push(e['parent']);
-    //      data.push(e['data']['from_id']);
-    //      data.push[data.length - 1].parent = e['parent'];
-    //  });
-
-    // var d = {id: root['repost_users'][root['repost_users'].length - 1]['data']['from_id']},
-    // parent = root['repost_users'][root['repost_users'].length - 1]['parent'];
-
-    var d = {id: data.length}, parent = data[~~(Math.random() * data.length)];
-
-    if (parent.children)
-        parent.children.push(d);
-    else
-        parent.children = [d];
-    data.push(d);
-
-    // Compute the new tree layout. We'll stash the old layout in the data.
-    var nodes = tree.nodes(rootTree);
-
-    // Update the nodes…
-    var node = vis.selectAll("circle.node")
-        .data(nodes, function(d) { return d.id; });
-
-    // Enter any new nodes at the parent's previous position.
-    node.enter().append("svg:circle")
-	.attr("class", "node")
-	.attr("r", 3.5)
-	.attr("cx", function(d) { return d.parent.x0; })
-	.attr("cy", function(d) { return d.parent.y0; })
-	.transition()
-	.duration(duration)
-	.attr("cx", x)
-	.attr("cy", y);
-
-    // Transition nodes to their new position.
-    node.transition()
-	.duration(duration)
-	.attr("cx", x)
-	.attr("cy", y);
-
-    // Update the links…
-    var link = vis.selectAll("path.link")
-	.data(tree.links(nodes), function(d) { return d.target.id; });
-
-    // Enter any new links at the parent's previous position.
-    link.enter().insert("svg:path", "circle")
-	.attr("class", "link")
-	.attr("d", function(d) {
-            var o = {x: d.source.x0, y: d.source.y0};
-            return diagonal({source: o, target: o});
-	})
-	.transition()
-	.duration(duration)
-	.attr("d", diagonal);
-
-    // Transition links to their new position.
-    link.transition()
-	.duration(duration)
-	.attr("d", diagonal);
-}
-
-function x(d) {
-    return d.x0 = d.x;
-}
-
-function y(d) {
-    return d.y0 = d.y;
 }
